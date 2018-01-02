0d9130b9964b662a2a718392e223c050549e9c57
jlebron143 <jlebron143@gmail.com>
2018-01-02 16:22:02 -0600

    removing method errors for switch case

--- RockPaperScissorsLizardSpock/RockPaperScissorsLizardSpock/Game.cs
+++ RockPaperScissorsLizardSpock/RockPaperScissorsLizardSpock/Game.cs
@@ -44,159 +44,161 @@ namespace RockPaperScissorsLizardSpock
 
             randomInt = rnd.Next(1, 6);
         }
+        public void RunGame (string inputPlayer, string inputCPU, int randomInt, int scoreCPU, int scorePlayer)
+        {
+            switch (randomInt)
+        {
+            case 1:
+                inputCPU = "Rock";
+                Console.WriteLine("Computer chose Rock");
+                if (inputPlayer == "Rock")
+                {
+                    Console.WriteLine("Draw!\n\n");
+                }
+                else if (inputPlayer == "PAPER")
+                {
+                    Console.WriteLine("Player Wins! Paper covers Rock!\n\n");
+                    scorePlayer++;
+                }
+                else if (inputPlayer == "Scissors")
 
-        //switch (randomInt)
-        //{
-        //    case 1:
-        //        inputCPU = "Rock";
-        //        Console.WriteLine("Computer chose Rock");
-        //        if (inputPlayer == "Rock")
-        //        {
-        //            Console.WriteLine("Draw!\n\n");
-        //        }
-        //        else if (inputPlayer == "PAPER")
-        //        {
-        //            Console.WriteLine("Player Wins! Paper covers Rock!\n\n");
-        //            scorePlayer++;
-        //        }
-        //        else if (inputPlayer == "Scissors")
+                {
+                    Console.WriteLine("CPU Wins! Rock smashes Scissors!\n\n");
+                    scoreCPU++;
+                }
+                else if (inputPlayer == "Lizard")
+                {
+                    Console.WriteLine("CPU Wins! Rock crushes Lizard!\n\n");
 
-        //        {
-        //            Console.WriteLine("CPU Wins! Rock smashes Scissors!\n\n");
-        //            scoreCPU++;
-        //        }
-        //        else if (inputPlayer == "Lizard")
-        //        {
-        //            Console.WriteLine("CPU Wins! Rock crushes Lizard!\n\n");
+                    scoreCPU++;
+                }
+                else if (inputPlayer == "Spock")
+                {
+                    Console.WriteLine("CPU Wins! Spock vaporizes Rock!\n\n");
+                    scoreCPU++;
+                }
+                break;
+            case 2:
+                inputCPU = "Paper";
+                Console.WriteLine("Computer chose Paper");
+                if (inputPlayer == "Paper")
+                {
+                    Console.WriteLine("Draw!\n\n");
+                }
+                else if (inputPlayer == "Rock")
+                {
+                    Console.WriteLine("CPU Wins! Paper covers Rock!\n\n");
+                    scoreCPU++;
+                }
+                else if (inputPlayer == "Scissors")
+                {
+                    Console.WriteLine("Player Wins! Scissors cuts Paper!\n\n");
+                    scoreCPU++;
+                }
+                else if (inputPlayer == "Lizard")
+                {
+                    Console.WriteLine("Player Wins! Lizard eats Paper!\n\n");
+                    scorePlayer++;
+                }
+                else if (inputPlayer == "Spock")
+                {
+                    Console.WriteLine("CPU Wins! Paper disproves Spock!\n\n");
+                    scoreCPU++;
+                }
+                break;
+            case 3:
+                inputCPU = "Scissors";
+                Console.WriteLine("Computer chose Scissors!");
+                if (inputPlayer == "Scissors")
+                {
+                    Console.WriteLine("Draw!\n\n");
 
-        //            scoreCPU++;
-        //        }
-        //        else if (inputPlayer == "Spock")
-        //        {
-        //            Console.WriteLine("CPU Wins! Spock vaporizes Rock!\n\n");
-        //            scoreCPU++;
-        //        }
-        //        break;
-        //    case 2:
-        //        inputCPU = "Paper";
-        //        Console.WriteLine("Computer chose Paper");
-        //        if (inputPlayer == "Paper")
-        //        {
-        //            Console.WriteLine("Draw!\n\n");
-        //        }
-        //        else if (inputPlayer == "Rock")
-        //        {
-        //            Console.WriteLine("CPU Wins! Paper covers Rock!\n\n");
-        //            scoreCPU++;
-        //        }
-        //        else if (inputPlayer == "Scissors")
-        //        {
-        //            Console.WriteLine("Player Wins! Scissors cuts Paper!\n\n");
-        //            scoreCPU++;
-        //        }
-        //        else if (inputPlayer == "Lizard")
-        //        {
-        //            Console.WriteLine("Player Wins! Lizard eats Paper!\n\n");
-        //            scorePlayer++;
-        //        }
-        //        else if (inputPlayer == "Spock")
-        //        {
-        //            Console.WriteLine("CPU Wins! Paper disproves Spock!\n\n");
-        //            scoreCPU++;
-        //        }
-        //        break;
-        //    case 3:
-        //        inputCPU = "Scissors";
-        //        Console.WriteLine("Computer chose Scissors!");
-        //        if (inputPlayer == "Scissors")
-        //        {
-        //            Console.WriteLine("Draw!\n\n");
+                }
+                else if (inputPlayer == "Rock")
+                {
+                    Console.WriteLine("Player Wins! Rock crushes Scissors!\n\n");
+                    scorePlayer++;
+                }
+                else if (inputPlayer == "Paper")
+                {
+                    Console.WriteLine("CPU Wins! Scissors cuts Paper!\n\n");
+                    scoreCPU++;
+                }
+                else if (inputPlayer == "Lizard")
+                {
+                    Console.WriteLine("CPU Wins! Scissors decapitates Lizard!\n\n");
+                    scoreCPU++;
+                }
+                else if (inputPlayer == "Spock")
+                {
+                    Console.WriteLine("Player Wins! Spock smashes Scissors!\n\n");
+                    scorePlayer++;
+                }
+                break;
 
-        //        }
-        //        else if (inputPlayer == "Rock")
-        //        {
-        //            Console.WriteLine("Player Wins! Rock crushes Scissors!\n\n");
-        //            scorePlayer++;
-        //        }
-        //        else if (inputPlayer == "Paper")
-        //        {
-        //            Console.WriteLine("CPU Wins! Scissors cuts Paper!\n\n");
-        //            scoreCPU++;
-        //        }
-        //        else if (inputPlayer == "Lizard")
-        //        {
-        //            Console.WriteLine("CPU Wins! Scissors decapitates Lizard!\n\n");
-        //            scoreCPU++;
-        //        }
-        //        else if (inputPlayer == "Spock")
-        //        {
-        //            Console.WriteLine("Player Wins! Spock smashes Scissors!\n\n");
-        //            scorePlayer++;
-        //        }
-        //        break;
+            case 4:
+                inputCPU = "Lizard";
+                Console.WriteLine("Computer chose Lizard!");
+                if (inputPlayer == "Lizard")
+                {
+                    Console.WriteLine("Draw!\n\n");
 
-        //    case 4:
-        //        inputCPU = "Lizard";
-        //        Console.WriteLine("Computer chose Lizard!");
-        //        if (inputPlayer == "Lizard")
-        //        {
-        //            Console.WriteLine("Draw!\n\n");
+                }
+                else if (inputPlayer == "Rock")
+                {
+                    Console.WriteLine("Player Wins!Rock crushes lizard!\n\n");
+                    scorePlayer++;
+                }
+                else if (inputPlayer == "Paper")
+                {
+                    Console.WriteLine("CPU Wins!Lizard eats Paper!\n\n");
+                    scoreCPU++;
+                }
+                else if (inputPlayer == "Scissors")
+                {
+                    Console.WriteLine("Player Wins! Scissors decapitates Lizard!\n\n");
+                    scorePlayer++;
+                }
+                else if (inputPlayer == "Spock")
+                {
+                    Console.WriteLine("Player Wins! Lizard poisons Spock!\n\n");
+                    scorePlayer++;
+                }
+                break;
 
-        //        }
-        //        else if (inputPlayer == "Rock")
-        //        {
-        //            Console.WriteLine("Player Wins!Rock crushes lizard!\n\n");
-        //            scorePlayer++;
-        //        }
-        //        else if (inputPlayer == "Paper")
-        //        {
-        //            Console.WriteLine("CPU Wins!Lizard eats Paper!\n\n");
-        //            scoreCPU++;
-        //        }
-        //        else if (inputPlayer == "Scissors")
-        //        {
-        //            Console.WriteLine("Player Wins! Scissors decapitates Lizard!\n\n");
-        //            scorePlayer++;
-        //        }
-        //        else if (inputPlayer == "Spock")
-        //        {
-        //            Console.WriteLine("Player Wins! Lizard poisons Spock!\n\n");
-        //            scorePlayer++;
-        //        }
-        //        break;
-
-        //    case 5:
-        //        inputCPU = "Spock";
-        //        Console.WriteLine("Computer chose Spock!");
-        //        if (inputPlayer == "Spock")
-        //        {
-        //            Console.WriteLine("DRAW!! \n\n");
-        //        }
-        //        else if (inputPlayer == "Rock")
-        //        {
-        //            Console.WriteLine("CPU Wins! Spock vaporizes Rock!\n\n");
-        //            scoreCPU++;
-        //        }
-        //        else if (inputPlayer == "Paper")
-        //        {
-        //            Console.WriteLine("Player Wins! Paper disproves Spock!\n\n");
-        //            scorePlayer++;
-        //        }
-        //        else if (inputPlayer == "Scissors")
-        //        {
-        //            Console.WriteLine("CPU Wins!Spock smashes Scissors!\n\n");
-        //            scoreCPU++;
-        //        }
-        //        else if (inputPlayer == "Lizard")
-        //        {
-        //            Console.WriteLine("Player Wins! Lizard Poisons Spock!!\n\n");
-        //            scorePlayer++;
-        //        }
-        //        break;
-        //    default:
-        //        Console.WriteLine("Invaild entry!");
-        //        break;
-        //}
+            case 5:
+                inputCPU = "Spock";
+                Console.WriteLine("Computer chose Spock!");
+                if (inputPlayer == "Spock")
+                {
+                    Console.WriteLine("DRAW!! \n\n");
+                }
+                else if (inputPlayer == "Rock")
+                {
+                    Console.WriteLine("CPU Wins! Spock vaporizes Rock!\n\n");
+                    scoreCPU++;
+                }
+                else if (inputPlayer == "Paper")
+                {
+                    Console.WriteLine("Player Wins! Paper disproves Spock!\n\n");
+                    scorePlayer++;
+                }
+                else if (inputPlayer == "Scissors")
+                {
+                    Console.WriteLine("CPU Wins!Spock smashes Scissors!\n\n");
+                    scoreCPU++;
+                }
+                else if (inputPlayer == "Lizard")
+                {
+                    Console.WriteLine("Player Wins! Lizard Poisons Spock!!\n\n");
+                    scorePlayer++;
+                }
+                break;
+            default:
+                Console.WriteLine("Invaild entry!");
+                break;
+        } }
+       
 
         public void Winner(int scorePlayer, int scoreCPU)
         {
